609715ec204ca422bce66920a631d7d9daaf044e
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-11 21:12:49 +0800

    add further validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,24 +1,28 @@
 class Book < ApplicationRecord
-    validates :title, presence:true, uniqueness: true, length: { minimum: 2 }
-    validates :author, presence:true, length: { minimum: 2 }
-    validates :sypnosis,length:  { maximum: 250,
-    too_long: "%{count} characters is the maximum allowed" }
-    validates :country, presence:true, length: { minimum: 2 }
-    validates :total_chapters, presence:true, numericality: { only_integer: true, greater_than_or_equal_to:0 }
-    validates :publication_date,  presence:true
+  validates :title, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :author, presence: true, length: { minimum: 2 }
+  validates :sypnosis, length: { maximum:  250,
+                                 too_long: "%<count>s characters is the maximum allowed" }
+  validates :country, presence: true, length: { minimum: 2 }
+  validates :total_chapters, presence:     true,
+                             numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :publication_date, presence: true
 
-    #additional validation
-    validate :publication_date_cannot_be_in_the_future
+  # additional validation
+  validate :publication_date_cannot_be_in_the_future
+  validate :country_must_start_with_capital_letter
 
-    private
-    def publication_date_cannot_be_in_the_future
-        if publication_date.present? && publication_date > Date.today
-          errors.add(:publication_date, "can't be in the future")
-        end
+  private
+
+  def publication_date_cannot_be_in_the_future
+    return unless publication_date.present? && publication_date > Time.zone.today
+
+    errors.add(:publication_date, "can't be in the future")
+  end
 
   def country_must_start_with_capital_letter
-    if country.present? && !country.match?(/\A[A-Z]/)
-      errors.add(:country, "must start with a capital letter")
-    end
+    return unless country.present? && !country.match?(/\A[A-Z]/)
+
+    errors.add(:country, "must start with a capital letter")
   end
 end
