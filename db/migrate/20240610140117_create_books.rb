9049804687ff110de37541dc341bad6ffc30a6dc
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-12 23:01:00 +0800

    Rename sypnosis to synopsis

--- db/migrate/20240610140117_create_books.rb
+++ db/migrate/20240610140117_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[7.1]
     create_table :books do |t|
       t.string :title
       t.string :author
-      t.text :sypnosis
+      t.text :synopsis
       t.date :publication_date
       t.string :country
       t.integer :total_chapters
