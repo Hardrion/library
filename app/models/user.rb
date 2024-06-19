e2037d46760f21d7e32d297e3e27073918cd9d88
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-20 00:24:29 +0800

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: {minimum: 2}
+    validates :last_name, presence: true, length:{minimum:2}
+    validates :email, presence:true, uniqueness: true, email: true
+    validates :date_of_birth, presence:true
+    validates :address, presence: true, length: { minimum: 2, maximum: 100 }
 end
