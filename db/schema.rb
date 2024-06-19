6c6be0764a915330b93c5116c4c34cfd24ec31d4
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-20 00:23:25 +0800

    add user and favorite tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_143143) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_19_145341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,25 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_143143) do
     t.index ["title"], name: "index_books_on_title", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_favorites_on_book_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.date "date_of_birth"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "favorites", "books"
+  add_foreign_key "favorites", "users"
 end
