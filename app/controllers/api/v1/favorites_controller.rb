542212a762f15d37ab8db723344273766625500f
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-21 01:53:57 +0800

    Add favorites controller documentation

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,10 +2,38 @@ class Api::V1::FavoritesController < Api::V1::BaseController
   before_action :set_user
   skip_before_action :verify_authenticity_token
 
-  # GET /api/v1/users/:user_id/favorites
+
+  api :GET, "/api/v1/users/:user_id/favorites", "Get user favorites"
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  param :page, :number, desc: "Page number for pagination"
+  example '
+  {
+    "favorite_books": [
+        {
+            "id": 62,
+            "title": "The Great Adventure",
+            "author": "John Smith",
+            "country": "USA",
+            "publication_date": "2020-01-15",
+            "total_chapters": 12
+        }
+    ],
+    "pagy": {
+        "current_page": 1,
+        "next_page": null,
+        "prev_page": null,
+        "total_pages": 1,
+        "total_count": 1
+    }
+  }
+  '
+
   def index
     @pagy, @favorites = pagy(@user.favorite_books, items: 5)
-    #render json: @favorites, status: :ok 
     render json: {
       favorite_books: ActiveModelSerializers::SerializableResource.new(@favorites,
                                                                each_serializer: BookSerializer),
@@ -14,6 +42,19 @@ class Api::V1::FavoritesController < Api::V1::BaseController
   end
   
 
+  api :POST, "/api/v1/users/:user_id/favorites", "Add book as user's favorite"
+  param :book_id, :number, desc: "Book to be added as favorite", required: true
+  returns code: 201, desc: "Created"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 409, desc: "Conflict"
+  returns code: 422, desc: "Unprocessable Entity"
+  returns code: 500, desc: "Internal Server Error"
+  example '
+  {
+    "book_id": 63
+  }
+  '
   def create
     book = Book.find(favorite_params[:book_id])
     existing_favorite = @user.favorites.find_by(book: book)
@@ -30,6 +71,14 @@ class Api::V1::FavoritesController < Api::V1::BaseController
     end
   end
 
+  api :DELETE, "/api/v1/users/:user_id/favorites/:book_id", "Delete book from favorites"
+  param :book_id, :number, desc: "ID of the user to be deleted"
+  returns code: 204, desc: "No Content"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "User not found"
+  returns code: 500, desc: "Internal Server Error"
+  
   def destroy
     @favorite = @user.favorites.find_by(book_id: params[:book_id])
 
