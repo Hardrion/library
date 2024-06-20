6bed53c28226441c67655eaa960ac3e3e0ab0c4b
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-21 01:54:06 +0800

    Add users controller documentation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,44 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user, only: [:show, :update, :destroy]
   skip_before_action :verify_authenticity_token
 
+  api :GET, "/api/v1/users", "Get users list"
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  param :page, :number, desc: "Page number for pagination"
+
+  example '
+  {
+    "users": [
+        {
+            "id": 43,
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "john.doe@example.com",
+            "date_of_birth": "1990-01-01",
+            "address": "123 Main St"
+        },
+        {
+            "id": 44,
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "email": "jane.smith@example.com",
+            "date_of_birth": "1985-05-05",
+            "address": "456 Elm St"
+        }
+    ],
+    "pagy": {
+        "current_page": 1,
+        "next_page": 2,
+        "prev_page": null,
+        "total_pages": 2,
+        "total_count": 10
+    }
+  }
+  '
+
   def index
     @pagy, @users = pagy(User.all, items: 5)
     #render json: {{users: @users,each_serializer: UserSerializer}, pagy: pagy_metadata(@pagy)}, , status: :ok
@@ -12,15 +50,53 @@ class Api::V1::UsersController < Api::V1::BaseController
     }
   end
 
+  
+  api :GET, "/api/v1/users/:id", "Show user"
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+
+  example '
+  {
+    "id": 45,
+    "first_name": "Alice",
+    "last_name": "Johnson",
+    "email": "alice.johnson@example.com",
+    "date_of_birth": "1992-03-12",
+    "address": "789 Pine St"
+  }
+  '
 
   def show
-    @user = User.find_by(id: params[:id])
     if @user.present?
-      render json: @user, serializer: UserSerializer
+      render json: @user, serializer: UserSerializer, status: :ok
     else
       render json: @user.as_json, status: :not_found
     end
   end
+
+  api :POST, "/api/v1/users", "Create user"
+  param :first_name, String, desc: "First name", required: true
+  param :last_name, String, desc: "Last name", required: true
+  param :email, String, desc: "Email address", required: true
+  param :date_of_birth, Date, desc: "Date of birth", required: true
+  param :address, String, desc: "Address", required: true
+  returns code: 201, desc: "Created"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 422, desc: "Unprocessable Entity"
+  returns code: 500, desc: "Internal Server Error"
+  example '
+   {
+    "first_name": "John",
+    "last_name": "Doe",
+    "email": "john.doe@gmail.com",
+    "date_of_birth": "2001-01-01",
+    "address": "Talisay City, Cebu"
+  }
+  '
 
   def create
     @user = User.new(user_params)
@@ -31,6 +107,29 @@ class Api::V1::UsersController < Api::V1::BaseController
     end
   end
 
+
+  api :PUT, "/api/v1/users/:id", "Create user"
+  param :id, :number, desc: "ID of the user"
+  param :first_name, String, desc: "First name"
+  param :last_name, String, desc: "Last name"
+  param :email, String, desc: "Email address"
+  param :date_of_birth, Date, desc: "Date of birth"
+  param :address, String, desc: "Address"
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 422, desc: "Unprocessable Entity"
+  returns code: 500, desc: "Internal Server Error"
+  example '
+  {
+   "first_name": "John",
+   "last_name": "Doe",
+   "email": "john.doe@gmail.com",
+   "date_of_birth": "2001-01-01",
+   "address": "Talisay City, Cebu"
+ }
+ '
+
   def update
     if @user.update(user_params)
       render json: @user, serializer: UserSerializer
@@ -38,6 +137,12 @@ class Api::V1::UsersController < Api::V1::BaseController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
+
+  api :DELETE, "/api/v1/users/:id", "Delete user"
+  param :id, :number, desc: "ID of the user to be deleted"
+  returns code: 204, desc: "No Content"
+  returns code: 404, desc: "User not found"
+  returns code: 500, desc: "Internal Server Error"
 
   def destroy
     @user.destroy
