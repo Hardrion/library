ad4481d3479fd241dc2c8c19868eca2916154de2
Hardelle Briones <hardelle.briones@gmail.com>
2024-06-21 01:54:20 +0800

    remove has many association in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :date_of_birth, :address
-  has_many :favorite_books, through: :favorites, source: :book
 end
